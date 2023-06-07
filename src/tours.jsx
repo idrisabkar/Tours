630f89d4ab5f97527dc5bcb1b8fc31aca93ccd0c
Idrissa <idrisabkar2018@gmail.com>
2023-06-08 00:19:59 +0300

    tours file added and modified

---
+++ src/tours.jsx
@@ -0,0 +1,44 @@
+export let tours = [
+  {
+    id: 1,
+    imageSrc: './images/bujumbura.jpg',
+    title: 'Best Of Bujumbura In 7 Days Tour',
+    content:
+      'Experience the beauty and charm of Bujumbura, the capital city of Burundi. Explore its rich culture, stunning landscapes, and vibrant markets.',
+  },
+  {
+    id: 2,
+    imageSrc: './images/konya.jpg',
+    title: 'Discover Konya: Sufism and Heritage',
+    content:
+      "Embark on a journey to Konya and delve into the mystical world of Rumi and Sufism. Visit the Mevlana Museum and explore the city's unique heritage.",
+  },
+  {
+    id: 3,
+    imageSrc: './images/kigali.jpeg',
+    title: 'Unforgettable Kigali: History and Landscapes',
+    content:
+      'Immerse yourself in the heart of Rwanda with a visit to Kigali. Discover its captivating history, vibrant markets, and breathtaking views of the surrounding hills.',
+  },
+  {
+    id: 4,
+    imageSrc: './images/kinshasa.jpg',
+    title: 'Kinshasa Adventure: Culture and Vibrancy',
+    content:
+      'Experience the vibrant energy of Kinshasa, the bustling capital city of the Democratic Republic of the Congo. Explore its lively markets, cultural landmarks, and vibrant music scene.',
+  },
+  {
+    id: 5,
+    imageSrc: './images/istanbul.jpg',
+    title: 'Istanbul Delights: East Meets West',
+    content:
+      'Indulge in the rich history and mesmerizing beauty of Istanbul. Visit iconic landmarks such as the Blue Mosque and Hagia Sophia, and immerse yourself in the vibrant atmosphere of the city.',
+  },
+  {
+    id: 6,
+    imageSrc: './images/nairobi.webp',
+    title: 'Nairobi Exploration: Wildlife and Culture',
+    content:
+      "Discover the wonders of Nairobi, the capital city of Kenya. Explore its national parks, bustling markets, and cultural sites, and get a taste of Kenya's vibrant urban life.",
+  },
+]
